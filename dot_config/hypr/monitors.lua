c4bb468a7ec09f397dbd778a2058753afa964072
Tori Dell <mx.tori.dell@gmail.com>
2026-08-11 04:00:08 -0400

    hyprland monitors

---
+++ dot_config/hypr/monitors.lua
@@ -0,0 +1,19 @@
+------------------
+---- MONITORS ----
+------------------
+
+-- See https://wiki.hypr.land/Configuring/Basics/Monitors/
+hl.monitor({
+	output = "eDP-1",
+	mode = "2560x1600@120",
+	position = "0x0",
+	scale = 1.6,
+})
+
+-- Rule to catch any new unrecognized monitors
+hl.monitor({
+	output = "",
+	mode = "preferred",
+	position = "auto",
+	scale = "auto",
+})
